abb47c32a26e309ba1abb59920385c0c1add5991
Jonathan-Unspecified <84442776+Jonathan-Unspecified@users.noreply.github.com>
2021-05-19 20:30:21 +0300

    second upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const MINE_IMG = '💣';
+const FLAG_IMG;
+const WIN_IMG;
+const LOSE_IMG;
+
+var gGame;
+var gBoard;
+var gLevel = {
+    size: 4,
+    mines: 2,
+    lives: 3
+};
+
+function init() {
+
+    var gGame = {
+        isOn = false,
+        shownCount = 0,
+        markedCount = 0,
+        secsPassed = 0
+    };
+
+    buildBoard();
+
+    renderBoard(gBoard);
+
+}
+
+function buildBoard() {
+    gBoard = createMat(gLevel.size, gLevel.size);
+
+
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            var currentCell = {
+                    minesAroundCount: 0,
+                    isShown: false,
+                    isMine: false,
+                    isMarked: false
+                }
+            gBoard[i][j] = currentCell;
+        }
+    }
+}
--- js/util.js
+++ js/util.js
@@ -1,1 +1,54 @@
 'use strict';
+
+function createMat(ROWS, COLS) {
+    var mat = []
+    for (var i = 0; i < ROWS; i++) {
+        var row = []
+        for (var j = 0; j < COLS; j++) {
+            row.push('')
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function copyMat(mat) {
+    var newMat = [];
+    for (var i = 0; i < mat.length; i++) {
+        newMat[i] = [];
+        for (var j = 0; j < mat[0].length; j++) {
+            newMat[i][j] = mat[i][j];
+        }
+    }
+    return newMat;
+}
+
+function printMat(mat, selector) {
+    var strHTML = '<table border="0"><tbody>';
+    for (var i = 0; i < mat.length; i++) {
+      strHTML += '<tr>';
+      for (var j = 0; j < mat[0].length; j++) {
+        var cell = mat[i][j];
+        var className = 'cell cell' + i + '-' + j;
+        strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+      }
+      strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>';
+    var elContainer = document.querySelector(selector);
+    elContainer.innerHTML = strHTML;
+  }
+
+  function renderCell(location, value) {
+    var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
+    elCell.innerHTML = value;
+  }
+
+
+
+  
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
